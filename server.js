99e503af625302b9f02578d94e02214ef89e0811
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-05-12 13:01:25 -0400

    modify custom middleware

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ app.use(logger('dev'))
 app.use(express.json())
 // app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:3000` }))
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE")
     next();
 });
 
